e092656c746ff33e98f97eb30335585f9f536f03
Dmitriy Dubson <pair+ddubson@pivotal.io>
2017-07-14 16:33:53 -0400

    Added ftp integration frame

---
+++ spring-integration/ftp-integration/src/main/java/com/ddubson/FilePrinter.java
@@ -0,0 +1,16 @@
+package com.ddubson;
+
+import java.io.*;
+
+public class FilePrinter {
+    public void print(File file) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            reader.lines().forEach(line -> System.out.println("[FILE-PARSED] " + line));
+        }
+    }
+
+    public void print(String fileContents) {
+        System.out.println("[FILE-AS-STRING] Invoking print method with a string.");
+        System.out.println("[FILE-AS-STRING] " + fileContents);
+    }
+}
