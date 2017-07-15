9648a961df46759b52d9071b1e95e802a1d8dbd9
Dmitriy Dubson <ddubson@pivotal.io>
2017-07-15 19:26:34 -0400

    Added basic FTP integration

--- spring-integration/ftp-integration/src/main/java/com/ddubson/FTPIntegrationApp.java
+++ spring-integration/ftp-integration/src/main/java/com/ddubson/FTPIntegrationApp.java
@@ -6,8 +6,8 @@ import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @ImportResource("integration-context.xml")
-public class FilesystemIntegrationApp {
+public class FTPIntegrationApp {
     public static void main(String[] args) {
-        SpringApplication.run(FilesystemIntegrationApp.class, args);
+        SpringApplication.run(FTPIntegrationApp.class, args);
     }
 }
