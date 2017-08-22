e632655b2dd3c9d4269430ed2b0613a2c754e342
Dmitriy Dubson <ddubson@pivotal.io>
2017-08-22 15:29:03 -0400

    Simplified integration flow; added partial gradle build files

--- spring-integration/basic-integration/src/main/kotlin/com/ddubson/integration/Application.kt
+++ spring-integration/basic-integration/src/main/kotlin/com/ddubson/integration/Application.kt
@@ -1,23 +1,20 @@
 package com.ddubson.integration
 
 import com.ddubson.ANSIColor
-import com.ddubson.CommandLineLogAdapter
 import com.ddubson.LogAdapter
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.context.annotation.Bean
 import org.springframework.integration.channel.DirectChannel
 import org.springframework.messaging.MessageHeaders
 import org.springframework.messaging.support.GenericMessage
 import org.springframework.messaging.support.MessageBuilder
 
 @SpringBootApplication
-open class Application(val outputChannel:DirectChannel,
-                       val inputChannel:DirectChannel,
-                       val logAdapter:LogAdapter) : ApplicationRunner {
+class Application(private val outputChannel: DirectChannel,
+                  private val inputChannel: DirectChannel,
+                  private val logAdapter: LogAdapter) : ApplicationRunner {
     override fun run(args: ApplicationArguments?) {
         val message = GenericMessage<Any>("Hello World", createMessageHeaders())
         val message2 = MessageBuilder
@@ -36,11 +33,9 @@ open class Application(val outputChannel:DirectChannel,
         val map: Map<String, Any?> = hashMapOf(Pair("key", "value"))
         return MessageHeaders(map)
     }
+}
 
-    companion object {
-        @JvmStatic fun main(args: Array<String>) {
-            SpringApplication.run(Application::class.java, *args)
-        }
-    }
+fun main(args: Array<String>) {
+    SpringApplication.run(Application::class.java, *args)
 }
 
--- spring-integration/basic-integration/src/main/kotlin/com/ddubson/integration/IntegrationConfig.kt
+++ spring-integration/basic-integration/src/main/kotlin/com/ddubson/integration/IntegrationConfig.kt
@@ -4,44 +4,38 @@ import com.ddubson.CommandLineLogAdapter
 import com.ddubson.LogAdapter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.integration.annotation.ServiceActivator
 import org.springframework.integration.channel.DirectChannel
 import org.springframework.integration.dsl.IntegrationFlow
 import org.springframework.integration.dsl.IntegrationFlows
 import org.springframework.integration.dsl.channel.MessageChannels
 
 @Configuration
-open class IntegrationgConfig {
-    @Bean("inputChannel")
-    open fun inputChannel(): DirectChannel {
-        return MessageChannels.direct().get()
-    }
-
-    @Bean("outputChannel")
-    open fun outputChannel(): DirectChannel {
-        return MessageChannels.direct().get()
-    }
-
+class IntegrationConfig {
     /**
      * Input Channel
      * -> Print Service (Service Activator)
      * -> Output Channel
      */
-    @Bean
-    open fun basicFlow(): IntegrationFlow {
+    //@Bean
+    /*fun basicFlow(): IntegrationFlow {
         return IntegrationFlows
                 .from(inputChannel())
                 .handle(printService())
                 .channel(outputChannel())
                 .get()
-    }
+    }*/
+
+    @Bean("inputChannel")
+    fun inputChannel(): DirectChannel = MessageChannels.direct().get()
+
+    @Bean("outputChannel")
+    fun outputChannel(): DirectChannel = MessageChannels.direct().get()
 
     @Bean
-    open fun logAdapter(): LogAdapter {
-        return CommandLineLogAdapter()
-    }
+    fun logAdapter(): LogAdapter = CommandLineLogAdapter()
 
     @Bean
-    open fun printService(): PrintService {
-        return PrintService(logAdapter())
-    }
+    @ServiceActivator(inputChannel = "inputChannel", outputChannel = "outputChannel")
+    fun printService(): PrintService = PrintService(logAdapter())
 }
